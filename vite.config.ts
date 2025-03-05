64f7fa6bb5b8103144f4b1d4b3f6bfa618a5692e
stnrvndev <stnrvndev>
2025-03-06 02:54:36 +0700

    Initial commit (2)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/atask/', // Sesuaikan dengan nama repository Anda
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
