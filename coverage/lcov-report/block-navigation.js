e686a26c6f58ace45af212d84f6f64d9f74fd85d
stnrvndev <stnrvndev>
2025-03-06 09:16:06 +0700

    fix: whatwg-fetch module

--- coverage/lcov-report/block-navigation.js
+++ coverage/lcov-report/block-navigation.js
@@ -1,11 +1,15 @@
 /* eslint-disable */
 var jumpToCode = (function init() {
+    // Classes of code we would like to highlight in the file view
     var missingCoverageClasses = ['.cbranch-no', '.cstat-no', '.fstat-no'];
 
+    // Elements to highlight in the file listing view
     var fileListingElements = ['td.pct.low'];
 
+    // We don't want to select elements that are direct descendants of another match
     var notSelector = ':not(' + missingCoverageClasses.join('):not(') + ') > '; // becomes `:not(a):not(b) > `
 
+    // Selecter that finds elements on the page to which we can jump
     var selector =
         fileListingElements.join(', ') +
         ', ' +
@@ -63,6 +67,7 @@ var jumpToCode = (function init() {
             document.getElementById('fileSearch') === document.activeElement &&
             document.activeElement != null
         ) {
+            // if we're currently focused on the search input, we don't want to navigate
             return;
         }
 
